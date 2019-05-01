370c469878e9217dca81bd2ead907fc582cd38a4
Madhur Dixit <dmadhur6@gmail.com>
2019-05-02 03:58:38 +0530

    AR libraries imported

--- app/src/main/java/com/example/android/shoppingpool/arActivity.java
+++ app/src/main/java/com/example/android/shoppingpool/arActivity.java
@@ -1,67 +1,204 @@
 package com.example.android.shoppingpool;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.android.shoppingpool.activities.MainActivity;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 public class arActivity extends AppCompatActivity {
+
+    private ArFragment fragment;
+    private PointerDrawable pointer = new PointerDrawable();
+    private boolean isTracking;
+    private boolean isHitting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
+        fragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
 
-        ImageView bed=(ImageView) findViewById(R.id.bed);
-        bed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"bed Clicked",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(arActivity.this, MainActivity.class);
-                intent.putExtra("object","bed");
-                startActivity(intent);
-
-            }
+        fragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+            fragment.onUpdate(frameTime);
+            onUpdate();
         });
 
-        ImageView couch=(ImageView) findViewById(R.id.couch);
-        couch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"couch Clicked",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(arActivity.this, MainActivity.class);
-                intent.putExtra("object","couch");
-                startActivity(intent);
-
-            }
-        });
-
-        ImageView computer=(ImageView) findViewById(R.id.computer);
-        computer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"computer Clicked",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(arActivity.this, MainActivity.class);
-                intent.putExtra("object","computer");
-                startActivity(intent);
-            }
-        });
-
-        ImageView rchair=(ImageView) findViewById(R.id.rchair);
-        rchair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"rchair Clicked",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(arActivity.this, MainActivity.class);
-                intent.putExtra("object","rchair");
-                startActivity(intent);
-            }
-        });
+        initializeGallery();
 
     }
+
+
+    private void onUpdate() {
+        boolean trackingChanged = updateTracking();
+        View contentView = findViewById(android.R.id.content);
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.getOverlay().add(pointer);
+            } else {
+                contentView.getOverlay().remove(pointer);
+            }
+            contentView.invalidate();
+        }
+
+        if (isTracking) {
+            boolean hitTestChanged = updateHitTest();
+            if (hitTestChanged) {
+                pointer.setEnabled(isHitting);
+                contentView.invalidate();
+            }
+        }
+    }
+
+    private boolean updateTracking() {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        boolean wasTracking = isTracking;
+        isTracking = frame != null && frame.getCamera().getTrackingState() == TrackingState.TRACKING;
+        return isTracking != wasTracking;
+    }
+
+    private boolean updateHitTest() {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        boolean wasHitting = isHitting;
+        isHitting = false;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    isHitting = true;
+                    break;
+                }
+            }
+        }
+        return wasHitting != isHitting;
+    }
+
+    private android.graphics.Point getScreenCenter() {
+        View vw = findViewById(android.R.id.content);
+        return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+    }
+
+    private void addObject(Uri model) {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    placeObject(fragment, hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
+        CompletableFuture<Void> renderableFuture =
+                ModelRenderable.builder()
+                        .setSource(fragment.getContext(), model)
+                        .build()
+                        .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                        .exceptionally((throwable -> {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage(throwable.getMessage())
+                                    .setTitle("Codelab error!");
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            return null;
+                        }));
+    }
+
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        String object = (String) b.get("object");
+
+
+        if (object.equals("bed")) {
+            ImageView bed = new ImageView(this);
+            bed.setImageResource(R.drawable.droid_thumb);
+            bed.setContentDescription("Bed_01");
+            bed.setOnClickListener(view -> {
+                addObject(Uri.parse("Bed_01.sfb"));
+            });
+            gallery.addView(bed);
+        }
+
+        if (object.equals("computer")) {
+            ImageView computer = new ImageView(this);
+            computer.setImageResource(R.drawable.cabin_thumb);
+            computer.setContentDescription("Computer");
+            computer.setOnClickListener(view -> {
+                addObject(Uri.parse("Computer.sfb"));
+            });
+            gallery.addView(computer);
+
+
+        }
+
+        if (object.equals("couch")) {
+            ImageView couch = new ImageView(this);
+            couch.setImageResource(R.drawable.house_thumb);
+            couch.setContentDescription("Couch");
+            couch.setOnClickListener(view -> {
+                addObject(Uri.parse("Couch.sfb"));
+            });
+            gallery.addView(couch);
+        }
+
+        if (object.equals("rchair")) {
+            ImageView rchair = new ImageView(this);
+            rchair.setImageResource(R.drawable.igloo_thumb);
+            rchair.setContentDescription("model");
+            rchair.setOnClickListener(view -> {
+                addObject(Uri.parse("model.sfb"));
+            });
+            gallery.addView(rchair);
+
+
+        }
+
+    }
+
+
 }
